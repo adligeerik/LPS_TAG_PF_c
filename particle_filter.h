5b8d35df48836cf6dc62f634e97917a5a9a35a07
Erik <erik.von.ke@gmail.com>
2019-08-01 13:59:47 +0200

    updated struct

--- particle_filter.h
+++ particle_filter.h
@@ -5,6 +5,9 @@
 
 struct particle {
 
+    int prevParticle;
+    int nextParticle;
+
     double x;
     double y;
     double z;
@@ -12,11 +15,28 @@ struct particle {
     double w;
 };
 
-struct ddist {
-
+struct ddist { 
+    char anchorname[6];
+    double ddist;
+    int ref_anchor;
 };
 
-struct anchorMap{
+struct anchor{
+    char anchorname[6];
+    double x;
+    double y;
+    double z;
+    int ref_anchor;
+};
+
+struct minmax{
+    double xmin;
+    double ymin;
+    double zmin;
+
+    double xmax;
+    double ymax;
+    double zmax;
 
 };
 
@@ -24,13 +44,13 @@ struct ddist calculate_ddist(struct particle particles[M],struct anchorMap ancho
 void move_particle(struct particle particles[M], double acceleration, int timestamp);
 void update_map(struct anchorMap anchorMap);
 void normalize_weight(struct particle particles[M]);
-double assign_weight(struct particle particles[M], struct anchorMap anchorMap, struct measurment measurment);
+double assign_weight(struct particle particles[M], struct anchorMap anchorMap, char * measurment);
 void low_variance_sampling(struct particle particles[M]);
 void highest_weight(struct particle particles[M]);
 void best_position(struct particle particles[M]);
-void particle_filter(struct particle particles[M], struct anchorMap anchorMap, char dataPackage);
+void particle_filter(struct particle particles[M], struct anchorMap anchorMap, char * dataPackage);
 void init(struct particle particles[M], struct minmax);
 
-void main();
+int main();
 
 #endif
